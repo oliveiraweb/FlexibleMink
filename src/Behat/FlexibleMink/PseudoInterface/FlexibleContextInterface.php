be141b2209fa063693ff2b1cb5447a16a3a8c948
crschoen <crschoen@gmail.com>
2016-04-13 13:53:11 -0500

    Added some basic assertion and actions

--- src/Behat/FlexibleMink/PseudoInterface/FlexibleContextInterface.php
+++ src/Behat/FlexibleMink/PseudoInterface/FlexibleContextInterface.php
@@ -13,6 +13,7 @@ trait FlexibleContextInterface
      * This method overrides the MinkContext::assertPageContainsText() default behavior for assertPageContainsText to
      * ensure that it waits for the text to be available with a max time limit.
      *
+     * @see MinkContext::assertPageContainsText
      * @param string $text Text to be searched in the page.
      */
     abstract public function assertPageContainsText($text);
@@ -24,4 +25,23 @@ trait FlexibleContextInterface
      * @return Session
      */
     abstract public function getSession($name = null);
+
+    /**
+     * This method overrides the MinkContext::assertPageNotContainsText() default behavior for assertPageNotContainsText
+     * to ensure that it waits for the item to not be available with a max time limit.
+     *
+     * @see MinkContext::assertPageNotContainsText
+     * @param string $text The text that should not be found on the page.
+     */
+    abstract public function assertPageNotContainsText($text);
+
+    /**
+     * Clicks a visible link with specified id|title|alt|text.
+     *
+     * This method overrides the MinkContext::clickLink() default behavior for clickLink to ensure that only visible
+     * links are clicked.
+     * @see MinkContext::clickLink
+     * @param string $locator The id|title|alt|text of the link to be clicked.
+     */
+    abstract public function clickLink($locator);
 }

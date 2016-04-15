3cd9a6e32b77d457a3defb6828424e7b910dea04
Chekote <domhnal.tilliere@gmail.com>
2016-04-15 16:22:08 -0500

    Added trait for capturing Screenshots.

---
+++ src/Behat/FlexibleMink/Context/ScreenShotContext.php
@@ -0,0 +1,48 @@
+<?php namespace Behat\FlexibleMink\PseudoInterface;
+
+use Behat\Behat\Hook\Scope\AfterStepScope;
+use Behat\Mink\Exception\UnsupportedDriverActionException;
+use Behat\Testwork\Tester\Result\TestResult;
+use features\contexts\MinkContextInterface;
+
+/**
+ * {@inheritdoc}
+ */
+trait ScreenShotContext
+{
+    // Depends
+    use MinkContextInterface;
+    use TestArtifactContextInterface;
+
+    // Implements
+    use ScreenShotContextInterface;
+
+    /**
+     * {@inheritdoc}
+     *
+     * @When /^(?:I )?take a screenshot(?: named "(?P<name>(?:[^"]|\\")*)")$/
+     */
+    public function takeScreenShot($name = 'screenshot')
+    {
+        file_put_contents(
+            $this->getArtifactsDir() . '/' . $name . '-' . date('Ymd-His-') . uniqid('', true) . '.png',
+            $this->getSession()->getDriver()->getScreenshot()
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function takeScreenShotAfterFailedStep(AfterStepScope $scope)
+    {
+        if (TestResult::FAILED === $scope->getTestResult()->getResultCode()) {
+            $name = str_replace(' ', '_', $scope->getFeature()->getTitle() . '-' . $scope->getStep()->getText());
+
+            try {
+                $this->takeScreenShot($name);
+            } catch (UnsupportedDriverActionException $e) {
+                // Silently ignore.
+            }
+        }
+    }
+}
---
+++ src/Behat/FlexibleMink/PseudoInterface/MinkContextInterface.php
@@ -0,0 +1,55 @@
+<?php namespace features\contexts;
+
+use Behat\Mink\Session;
+
+/**
+ * Pseudo interface for tracking the methods of the MinkContext.
+ *
+ * @see Behat\MinkExtension\Context\MinkContext.
+ */
+trait MinkContextInterface
+{
+    /**
+     * Checks that page contains the specified text.
+     *
+     * @param string $text The text that should be found on the page.
+     */
+    abstract public function assertPageContainsText($text);
+
+    /**
+     * Checks the checkbox with specified id|name|label|value.
+     *
+     * @param string $option The identifier of the option
+     */
+    abstract public function checkOption($option);
+
+    /**
+     * Fills in form input with specified id|name|label|value.
+     *
+     * @param string $field the input id, name or label
+     * @param string $value the value to fill
+     */
+    abstract public function fillField($field, $value);
+
+    /**
+     * Returns the Mink session.
+     *
+     * @param  string|null $name name of the session OR active session will be used
+     * @return Session
+     */
+    abstract public function getSession($name = null);
+
+    /**
+     * Presses the button with specified id|name|title|alt|value.
+     *
+     * @param string $button button id, value or alt
+     */
+    abstract public function pressButton($button);
+
+    /**
+     * Opens specified page.
+     *
+     * @param string $page The URL to visit.
+     */
+    abstract public function visit($page);
+}

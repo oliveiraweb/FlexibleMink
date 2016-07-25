45bc0c3423150de69cb14a5a350679203f3ca0df
Josh Medeski <josh@medology.com>
2016-07-25 14:34:42 -0500

    Add line break under inheritdoc tag for coding consistency

--- src/Behat/FlexibleMink/Context/FlexibleContext.php
+++ src/Behat/FlexibleMink/Context/FlexibleContext.php
@@ -340,6 +340,7 @@ class FlexibleContext extends MinkContext
 
     /**
      * {@inheritdoc}
+     *
      * @When /^(?:I |)(?:blur|unfocus) (?:the |)"(?P<locator>[^"]+)"(?: field|)$/
      */
     public function blurField($locator)
@@ -353,6 +354,7 @@ class FlexibleContext extends MinkContext
 
     /**
      * {@inheritdoc}
+     *
      * @When /^(?:I |)focus and (?:blur|unfocus) (?:the |)"(?P<locator>[^"]+)"(?: field|)$/
      * @When /^(?:I |)toggle focus (?:on|of) (?:the |)"(?P<locator>[^"]+)"(?: field|)$/
      */
@@ -364,6 +366,7 @@ class FlexibleContext extends MinkContext
 
     /**
      * {@inheritdoc}
+     *
      * @When /^(?:I |)focus (?:the |)"(?P<locator>[^"]+)"(?: field|)$/
      */
     public function focusField($locator)

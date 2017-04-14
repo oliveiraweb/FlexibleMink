80c89d8839e81533ab3af0f479305e783b4ab8f4
Josh Medeski <joshmedeski@users.noreply.github.com>
2017-04-14 09:49:34 -0600

    Apply fixes from StyleCI (#106)

--- src/Behat/FlexibleMink/PseudoInterface/FlexibleContextInterface.php
+++ src/Behat/FlexibleMink/PseudoInterface/FlexibleContextInterface.php
@@ -31,7 +31,7 @@ trait FlexibleContextInterface
      * Asserts that the page contains a list of strings.
      *
      * @param  TableNode             $table The list of strings to find.
-     * @param  String                $not   A flag to assert not containing text.
+     * @param  string                $not   A flag to assert not containing text.
      * @throws ResponseTextException If the text is not found.
      */
     abstract public function assertPageContainsTexts(TableNode $table, $not = null);

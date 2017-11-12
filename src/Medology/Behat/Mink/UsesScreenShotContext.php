5c58924c49c762a435108118f3812d195dd5422c
Donald Tyler <domhnall.tilliere@gmail.com>
2017-11-12 16:33:49 -0600

    Add UsesScreenShotContext trait

---
+++ src/Medology/Behat/Mink/UsesScreenShotContext.php
@@ -0,0 +1,38 @@
+<?php namespace Medology\Behat\Mink;
+
+use Behat\Behat\Context\Environment\InitializedContextEnvironment;
+use Behat\Behat\Hook\Scope\BeforeScenarioScope;
+use RuntimeException;
+
+/**
+ * Trait that grants access to the ScreenShotContext instance via $this->screenShotContext.
+ */
+trait UsesScreenShotContext
+{
+    /** @var ScreenShotContext */
+    protected $screenShotContext;
+
+    /**
+     * Gathers the ScreenShotContext instance and stores a reference in $this->screenShotContext.
+     *
+     * @param  BeforeScenarioScope $scope
+     * @throws RuntimeException    If the current environment is not initialized.
+     * @return void
+     * @BeforeScenario
+     */
+    public function gatherScreenShotContext(BeforeScenarioScope $scope)
+    {
+        $environment = $scope->getEnvironment();
+
+        if (!($environment instanceof InitializedContextEnvironment)) {
+            throw new RuntimeException(
+                'Expected Environment to be ' . InitializedContextEnvironment::class .
+                ', but got ' . get_class($environment)
+            );
+        }
+
+        if (!$this->screenShotContext = $environment->getContext(ScreenShotContext::class)) {
+            throw new RuntimeException('Failed to gather ScreenShotContext');
+        }
+    }
+}

f5e0414409af18fcfcfa0e2a07f123d7f6160598
Johnnie Zhang <johnnie502@gmail.com>
2018-07-06 11:09:42 -0500

    Upgrade to Circle CI 2.0 For 1.0 Branch (#161)
    * Upgrade Circle CI to 2.0
    
    * code review change
    
    * fix branch name

--- bin/init/init_docker_env.sh
+++ bin/init/init_docker_env.sh
@@ -1,20 +1,16 @@
-#!/bin/bash +x
+#!/usr/bin/env bash
 
-ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../" && pwd )"
+ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )"
 
-. "${ROOT}"/bin/lib/env.sh
+. "${ROOT}"/bin/lib/exitCheck.sh
 
 echo "Checking .env file..."
 if [ ! -e "$ROOT"/docker/.env ]; then
   echo "No docker/.env file found. Creating from .env.example..."
   cp "$ROOT"/docker/.env.example "$ROOT"/docker/.env
+  exitCheck $?
   echo "NOTE: Please review the docker/.env file and configure any required parameters."
 else
   echo "docker/.env file exists. Skipping create."
-  echo "NOTE: Please review the docker/.env file and configure any required parameters."
+  echo "NOTE: Please review the docker/.env.example file and ensure all required parameters are present in docker/.env"
 fi
-
-echo "Working on containers..."
-docker-compose -p $PROJECT_NAME \
-    -f "$ROOT"/docker/docker-compose.yml \
-    "$@"

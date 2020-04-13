4a8c9ad15cb0ce16f64b06cd3015809d487e47aa
DevanTarunKumar <37919626+DevanTarunKumar@users.noreply.github.com>
2020-04-13 13:45:05 -0500

    upgrading composer (#336)

--- docker/lib/images.sh
+++ docker/lib/images.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
+export DOCKER_REGISTRY_HOST=registry.hub.docker.com
+
 export PHP_OWNER=chekote
 export PHP_REPO=php
 export PHP5_TAG=5.6.40.b-behat3.4
 export PHP7_TAG=7.2.16-behat3.4
 
-export PHP5_IMAGE=${PHP_OWNER}/${PHP_REPO}:${PHP5_TAG}
-export PHP7_IMAGE=${PHP_OWNER}/${PHP_REPO}:${PHP7_TAG}
+export PHP5_IMAGE=${DOCKER_REGISTRY_HOST}/${PHP_OWNER}/${PHP_REPO}:${PHP5_TAG}
+export PHP7_IMAGE=${DOCKER_REGISTRY_HOST}/${PHP_OWNER}/${PHP_REPO}:${PHP7_TAG}
+
+export SED_ACCOUNT=library
+export SED_REPO=alpine
+export SED_TAG=3.11.3
+export SED_IMAGE=${DOCKER_REGISTRY_HOST}/${SED_ACCOUNT}/${SED_REPO}:${SED_TAG}
--- docker/lib/php_image.sh
+++ docker/lib/php_image.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-set -u
+set -eu
 
-if [ -z ${USE_PHP5:-} = "1" ]; then
+if [ "${USE_PHP5:='false'}" == true ]; then
     export RESOLVED_PHP_IMAGE=${PHP5_IMAGE}
 else
     export RESOLVED_PHP_IMAGE=${PHP7_IMAGE}

3fb0f1a6e9473d8e9517bb77a7ba4ae73ff41769
Andrew Servi <andrew@healthlabs.com>
2021-07-09 10:58:33 -0500

    Update PHP image (#375)

--- docker/lib/images.sh
+++ docker/lib/images.sh
@@ -5,7 +5,7 @@ export DOCKER_REGISTRY_HOST=registry.hub.docker.com
 export PHP_OWNER=chekote
 export PHP_REPO=php
 export PHP5_TAG=5.6.40.b-behat3.4
-export PHP7_TAG=7.2.16-behat3.4
+export PHP7_TAG=7.2.34.d-behat3.4
 
 export PHP5_IMAGE=${DOCKER_REGISTRY_HOST}/${PHP_OWNER}/${PHP_REPO}:${PHP5_TAG}
 export PHP7_IMAGE=${DOCKER_REGISTRY_HOST}/${PHP_OWNER}/${PHP_REPO}:${PHP7_TAG}
